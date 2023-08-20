a7918a688cfeb55f58249c11ca826975926a3622
Mohammed Farish <farish@Mohammeds-MacBook-Pro.local>
2023-08-20 13:35:43 +0400

    update to mobile number

--- src/config/contactInfo.js
+++ src/config/contactInfo.js
@@ -1,6 +1,6 @@
 const contactInfo = {
   mainEmail: "consult@growmoreinternational.com",
-  phone: "+971 55 617 8423",
+  phone: "+971 55 874 2358",
   location: "Abu Dhabi, United Arab Emirates",
 };
 
